80cc72c10d9e0e951b94e2e65373da5a7fb0d30c
itshadis <hadis1098@gmail.com>
2024-05-01 11:27:07 +0700

    handlesubmit login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import loginImg from '../../assets/logo-s.png'
 import { useState } from "react";
@@ -7,11 +6,9 @@ const Login = () => {
   const [email, setEmail] = useState()
   const [pass, setPass] = useState()
   
-  // const handleSubmit = () => {
-  //   alert(email, pass)
-  // }
-
-
+  const handleSubmit = () => {
+    alert(email, pass)
+  }
 
   return (<section className="bg-gray-50 ">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -24,13 +21,13 @@ const Login = () => {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                   Sign in to your account
               </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
+              <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
                   <div>
-                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                      <label className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
                       <input onChange={(e)=> setEmail(e.target.value)} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="email account" required=""/>
                   </div>
                   <div>
-                      <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                       <input onChange={(e)=> setPass(e.target.value)} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required=""/>
                   </div>
                   <div className="flex items-center justify-between">
@@ -39,7 +36,7 @@ const Login = () => {
                             <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 " required=""/>
                           </div>
                           <div className="ml-3 text-sm">
-                            <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                            <label className="text-gray-500 dark:text-gray-300">Remember me</label>
                           </div>
                       </div>
                       <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
